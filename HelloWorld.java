1fdf86d52dc03421e5905aca3db8f68368af6cdf
Subrahmanyam Mamidi <msminus2017@gmail.com>
2025-04-13 04:35:29 +0000

    First commit with vscode server

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Github in VSCode server");
+
+        ContactDetails cDetails = new ContactDetails();
+        cDetails.setEmail("msminus2017@gmail.com");
+        cDetails.setName("Subrahmanyam Mamidi");
+
+        System.out.println(cDetails.getEmail() + " \n " + cDetails.getName());
+        System.out.println("Calling external class is over");
+
+        System.out.println("Staged and Modified");
+        //Git recognizes as exting file and is updated. Showing "M".
+        //In the begining it showed as "U" untracking
+        //After adding to staged it showed "A"
+    }
+}
